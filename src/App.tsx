2494fa72f2557e4c3cfa0a9d0ea676051f6e570e
Sergey Chumak <sergey.chumak.94@gmail.com>
2023-02-19 21:28:05 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,1 @@
-import React from 'react';import './App.scss';const App = () => <div className="app2">App</div>;export default App;
+import React from 'react';import './App.scss';const App = () => <div className="app">App</div>;export default App;
