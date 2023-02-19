7059fd5ed74d50f3e8ab0203302064c31ad3b4d1
Sergey Chumak <sergey.chumak.94@gmail.com>
2023-02-19 19:24:18 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,6 @@
 import React from 'react';
 import './App.scss';
 
-const App2 = (props: any) => {
-  const {
-    prop1,
-    prop2,
-    props3,
-    prop4,
-    prop5,
-    prop6,
-    prop7,
-    prop8,
-    prop9,
-    prop10,
-  } = props;
-
-  const arr = [
-    { id: 1, name: 'App', template: 4, teafwfwaf: 3, dwadadw: 9 },
-    { id: 1 },
-
-    { id: 1 },
-  ];
-
-  console.log(
-    arr,
-    prop1,
-    prop2,
-    props3,
-    prop4,
-    prop5,
-    prop6,
-    prop7,
-    prop8,
-    prop9,
-    prop10
-  );
-
-  return <div />;
-};
-
-const App = () => (
-  <div
-    className="App"
-    onChange={() => {}}
-  >
-    <App2 />
-  </div>
-);
+const App = () => <div className="App">App</div>;
 
 export default App;
