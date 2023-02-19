506e498f2cdff70371e50e272aadcd4fa3d6ef1e
Sergey Chumak <sergey.chumak.94@gmail.com>
2023-02-19 20:37:27 +0300

    skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,1 @@
-import React from 'react';
-import './App.scss';
-
-const App = () => <div className="App">App</div>;
-
-export default App;
+import React from 'react';import './App.scss';const App = () => <div className="app">App</div>;export default App;
