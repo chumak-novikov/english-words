b11ddc05b2dde3fde48b5dca4643f5b00e992f63
Sergey Chumak <87362741+sergey-chumakv@users.noreply.github.com>
2023-02-21 23:01:45 +0300

    EW-8 Добавлен компонент гбаттон груп (#5)

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button as MUIButton } from '@mui/material';
 
+import classNames from 'classnames';
 import styles from './Button.module.scss';
 
 type ButtonProps = {
@@ -9,6 +10,7 @@ type ButtonProps = {
   onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   type?: 'button' | 'submit';
   color?: 'contained' | 'text';
+  fullWidth?: boolean;
   className?: string;
 };
 
@@ -19,28 +21,28 @@ export const Button = (props: ButtonProps) => {
     onClick,
     children,
     disabled,
+    fullWidth,
     className: externalClassName,
   } = props;
 
   const buttonClasses = {
-    root: styles.root,
+    root: classNames(styles.root, externalClassName),
     contained: styles.button,
     text: styles.text,
     disabled: styles.disabled,
   };
 
   return (
-    <div className={externalClassName}>
-      <MUIButton
-        variant={color}
-        disabled={disabled}
-        type={type}
-        onClick={onClick}
-        classes={buttonClasses}
-        size="large"
-      >
-        {children}
-      </MUIButton>
-    </div>
+    <MUIButton
+      fullWidth={fullWidth}
+      variant={color}
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+      classes={buttonClasses}
+      size="large"
+    >
+      {children}
+    </MUIButton>
   );
 };
