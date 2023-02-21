52eea7f655d8794bdb1705f40b8035acd6d26f19
Sergey Chumak <sergey.chumak.94@gmail.com>
2023-02-21 15:48:16 +0300

    EW-2 ДобаÐавлен комонент кнопки

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,51 @@
-import React from 'react';import './App.scss';import { Input } from './shared/ui/Input';import { Button } from './shared/ui/Button';const App = () => (  <div className="app">    <Input      label="Label"      required      value="Sergey"    />    <Input      label="Label"      required    />    <Input label="Label" />    <Input      label="Label"      error    />    <Input      label="Label"      error      value="Dima chiks"    />    <Input      label="Label"      className="input"      value="Dima chiks"    />    <Button>Button</Button>    <Button disabled>Button</Button>    <Button color="text">Button</Button>    <Button      color="text"      disabled    >      Button    </Button>  </div>);export default App;
+import React from 'react';
+
+import './App.scss';
+import { Input } from './shared/ui/Input';
+import { Button } from './shared/ui/Button';
+
+const App = () => (
+  <div className="app">
+    <Input
+      label="Label"
+      required
+      value="Sergey"
+    />
+
+    <Input
+      label="Label"
+      required
+    />
+
+    <Input label="Label" />
+
+    <Input
+      label="Label"
+      error
+    />
+
+    <Input
+      label="Label"
+      error
+      value="Dima chiks"
+    />
+
+    <Input
+      label="Label"
+      className="input"
+      value="Dima chiks"
+    />
+
+    <Button>Button</Button>
+    <Button disabled>Button</Button>
+    <Button color="text">Button</Button>
+    <Button
+      color="text"
+      disabled
+    >
+      Button
+    </Button>
+  </div>
+);
+
+export default App;
